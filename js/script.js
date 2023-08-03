590bc75b96d19ed125bbecaaf392c5b35c89d6b7
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-08-02 22:34:37 -0700

    finish state functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-import Player from "/js/player.js";
+import Player from "./player.js";
+import InputHandler from "./input.js";
+import { drawStatusText } from "./utils.js";
 
 window.addEventListener("load", function () {
   const loading = document.getElementById("loading");
@@ -10,5 +12,17 @@ window.addEventListener("load", function () {
 
   const player = new Player(canvas.width, canvas.height);
   console.log(player);
-  player.draw(ctx);
+  const input = new InputHandler();
+
+  let lastTime = 0;
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update(input.lastKey);
+    player.draw(ctx, deltaTime);
+    drawStatusText(ctx, input, player);
+    requestAnimationFrame(animate);
+  }
+  animate(1);
 });
